11230de117f42a555bbf59bcced5609317594ba5
SahayakSharma <sahayaksharma6@gmail.com>
2023-12-11 18:56:31 +0530

    modified

--- index.jsx
+++ index.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 const mybutton=()=>{
     return(
         <button style={{padding:"15px", backgroundColor:"black" ,color:"white",fontWeight:"bold"}}>Start the test</button>
