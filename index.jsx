ac7f0df4a7c8f45eb81c9002ec5e7daa1ba94974
SahayakSharma <sahayaksharma6@gmail.com>
2023-12-11 18:46:28 +0530

    initial commit

---
+++ index.jsx
@@ -0,0 +1,9 @@
+
+const mybutton=()=>{
+    return(
+        <button style={{padding:"15px", backgroundColor:"black" ,color:"white",fontWeight:"bold"}}>Start the test</button>
+    )
+}
+
+
+export default mybutton;
